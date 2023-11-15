a1b771b7f40e73bc24fe57f8e1845df0da56d834
Sinchi <truskovsky2017@yandex.ru>
2023-11-15 20:02:27 +0300

    lab3 fin

--- 3 лаба/3 лаба 1 задание.py
+++ 3 лаба/3 лаба 1 задание.py
@@ -1,15 +1,15 @@
 import re
 def Search(str_input):
-    count = re.findall(r'=|-{|p',str_input)
+    count = re.findall(r'=-{p',str_input)
     return len(count)
     
 
-print(Search('pasdlalskdapw#@(@#$)@Isqppp=021o3-}')) #6    
-print(Search('-0123paos[po2-=130psl;-}202002skaldp12=]')) 
-print(Search('a;lgf[dspogs0aor[]ewpe=1][SAL;]')) 
-print(Search('ydgaoXLK:,ms;lrtauiow[pdla;s')) 
-print(Search('GHFDJMTNRYUSKDFJNSMI12093I21PO90S')) 
-alph = ['=','-{','p']
+print(Search('pasdlalskdap=-{pw#@(@#$)@Isqppp=021o3-}')) #6    
+print(Search('-0123paos[po2-=13=-{p0psl;-}202=-{p=-{p002skaldp12=]')) 
+print(Search('a;=-{plgf[dspogs0aor[]ewpe=1][SAL;]')) 
+print(Search('ydgaoXLK:,ms;lrtauiow[pdla;s')) #1
+print(Search('GHFDJM=-{pTNRYUSKDFJNSMI12093I21PO90S')) 
+alph = ['=-{p']
 
 
 
--- 3 лаба/3 лаба 3 задание.py
+++ 3 лаба/3 лаба 3 задание.py
@@ -1,18 +1,24 @@
 import re 
 
 def matches(str_input):
-    a = [a.group() for a in re.finditer( r'(\w+)\-\1\s[А-Я]\.[А-Я].\sР\d{4}',str_input)]
-    b = [b.group() for b in re.finditer( r'\w+\s([А-Я])\.\1\.\s[Р]\d{4}', str_input)]
+    c = [a.group() for a in re.finditer(r'(\w+)\-\1\s[А-Я]\.[А-Я].\sР(3114)',str_input)]
+    d = [b.group() for b in re.finditer(r'\w+\s([А-Я])\.\1\.\sР(3114)', str_input)]
+    a = re.sub(r'(\w+)\-\1\s[А-Я]\.[А-Я].\sР(3114)',' (TBD) ',str_input)
+    a = re.sub(r'\w+\s([А-Я])\.\1\.\sР(3114)',' (TBD) ',a)
+    return print(a,c,d)
+    
+    '''
     if len(a) > 0 or len(b) > 0:
         if len(a) > 0 and len(b) == 0:
             return print(a)
-        if len(a) == 0 and len(b) > 0:
+        if len(a) == 0 and len(b) > 0: 
             return print(b)
         else: return print(a,b)
     else: return print('Выражений не найдено!')
+    '''
 
 matches('Трусковский Г.А. Р3114 Гофман И.Н. Р7878 Спартак Н.В. Р5678') # 0
-matches('Трусковский Г.А. Р3114 Федченко К.К. Р3114 Рыжик М.М. Р3245') # Федченко + Рыжик
-matches('Трусковский Г.А. Р3114 Гофман И.Н. Р7878 Спартак-Спартак Н.В. Р5678') # Cпартак
+matches('Трусковский-Трусковский Г.А. Р3114 Федченко К.К. Р3114 Рыжик М.М. Р3245') # Федченко + Рыжик
+matches('Трусковский Г.А. Р3114 Гофман И.И. Р7878 Спартак-Спартак Н.В. Р5678') # Cпартак
 matches('Трусковский Г.А. Р3114 Гофман-Гофман И.Н. Р7878 Огарков И.И. Р5678') # Гофман+Огарков
 matches('Лапинский С.С. Р3114 Сепьев-Сепьев К.Н. Р7878 Цветков И.И. Р5678') # Все
